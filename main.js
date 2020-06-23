64ddd0483502d5e82dbd4d0e70ed0e3f0395feee
O-maniac <62053186+O-maniac@users.noreply.github.com>
2020-06-23 15:06:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const searchBox = document.querySelector(".search-box");
+const navbuttoncontainer = document.querySelector("nav-button-container");
+const searchbtn = document.querySelector(".search-btn");
+const closebtn = document.querySelector(".close-btn");
+
